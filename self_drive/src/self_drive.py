6de110f8e9f227e985ff6b70f4a0c9793e3ba9ab
burger_06 <burger_06@robotics.ac.kr>
2020-12-06 08:48:54 +0900

    전역변수를 이용하여 mod의 이름을 지음

--- self_drive/src/self_drive.py
+++ self_drive/src/self_drive.py
@@ -4,22 +4,26 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+INTRO = 0
+FOLLOW_CHECK = 1
+TURN = 2
+
 class SelfDrive:
 
     def __init__(self, publisher):
         self.publisher = publisher
-        self.mod = 0
+        self.mod = INTRO
 
     def lds_callback(self, scan):
         turtle_vel = Twist()
         
-        if self.mod == 0:
+        if self.mod == INTRO:
             if 0 < scan.ranges[0] < 0.25 or 0 < scan.ranges[30] < 0.25 or 0 < scan.ranges[-30] < 0.25:
-                self.mod = 1
+                self.mod = FOLLOW_CHECK
             else:
                 turtle_vel.linear.x = 0.18
         
-        elif self.mod == 1:
+        elif self.mod == FOLLOW_CHECK:
             if 0 < scan.ranges[0] < 0.25 or 0 < scan.ranges[30] < 0.25 or 0 < scan.ranges[-30] < 0.25:
                 turtle_vel.angular.z = -1.8
             elif abs(scan.ranges[110] - scan.ranges[70]) > 0.02 and (scan.ranges[110] - scan.ranges[70]) > 0 \
@@ -29,17 +33,17 @@ class SelfDrive:
             elif abs(scan.ranges[110] - scan.ranges[70]) > 0.02 and (scan.ranges[110] - scan.ranges[70]) < 0 \
                     or scan.ranges[90] > 0.2:
                 if scan.ranges[110] - scan.ranges[70] < -0.04 and scan.ranges[110] != 0:
-                    self.mod = 2
+                    self.mod = TURN
                 turtle_vel.angular.z = 0.7
                 turtle_vel.linear.x = 0.18
             else:
                 turtle_vel.linear.x = 0.18
                 
-        elif self.mod == 2:
+        elif self.mod == TURN:
             turtle_vel.angular.z = 1.5
             turtle_vel.linear.x = 0.15
             if 0 < scan.ranges[0] < 0.22 or 0 < scan.ranges[30] < 0.25 or 0 < scan.ranges[45] < 0.25:
-                self.mod = 1
+                self.mod = FOLLOW_CHECK
                 
         self.publisher.publish(turtle_vel)
     
